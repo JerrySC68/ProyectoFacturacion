e3e8df772bf12fed8e70b1ce56198020d8e3b61b
JerrySC68 <jerrysc68@gmail.com>
2024-04-13 23:11:06 -0600

    Cambios Factura

--- src/main/java/org/example/prograivproyectoi/presentation/Controller/FacturaController.java
+++ src/main/java/org/example/prograivproyectoi/presentation/Controller/FacturaController.java
@@ -1,20 +1,65 @@
 package org.example.prograivproyectoi.presentation.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.example.prograivproyectoi.logic.Model.Cliente;
+import org.example.prograivproyectoi.logic.Model.Factura;
 import org.example.prograivproyectoi.logic.Model.Producto;
 import org.example.prograivproyectoi.logic.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
+@RequestMapping("/presentantion/factura/listaFactura")
 public class FacturaController {
     @Autowired
     private Service service;
+    @Autowired
+    LocaleResolver localeResolver;
+
+    @GetMapping({"", "/presentantion/factura/listaFactura"})
+    public String showProductosList(Model model, HttpServletRequest request, HttpServletResponse response,
+                                    @RequestParam(name = "lang", required = false) String lang)
+    {
+        //--------------------------------------------------------------------------------
+        // Multi lenguaje
+        //--------------------------------------------------------------------------------
+        if (lang != null)
+        {
+            localeResolver.setLocale(request, response, new Locale(lang));
+        }
+        List<Factura> facturas = service.findAllFacturas();
+
+        model.addAttribute("facturas", facturas);
+
+        return "presentantion/factura/listaFactura";
+    }
+
+    @GetMapping("/FacturaMain")
+    public String showFacturaCreatePage(Model model, HttpServletRequest request, HttpServletResponse response,
+                                         @RequestParam(name = "lang", required = false) String lang)
+    {
+        //--------------------------------------------------------------------------------
+        // Multi lenguaje
+        //--------------------------------------------------------------------------------
+        if (lang != null) {
+            localeResolver.setLocale(request, response, new Locale(lang));
+        }
+
+        Factura factura = new Factura();
+        model.addAttribute("factura", factura);
+
+        return "/presentantion/factura/FacturaMain";
+    }
 
     @Autowired
     public FacturaController(Service service) {
