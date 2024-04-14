08aed25febd074a8c3a72830342fe61997b52d37
RodrigoACR <rodrigo.castro.rojas@est.una.ac.cr>
2024-04-13 22:01:46 -0600

    Correccion Unificacion - Producto

--- src/main/java/org/example/prograivproyectoi/logic/Model/Producto.java
+++ src/main/java/org/example/prograivproyectoi/logic/Model/Producto.java
@@ -12,19 +12,33 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "productos")
-public class Producto {
+public class Producto
+{
+    //-----------------------------------------------------------------------------------------
+    //Atributos
+    //-----------------------------------------------------------------------------------------
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    @NotNull(message = "{error.Empty}")
     private Boolean type;
+    @NotEmpty(message = "{error.Empty}")
     private String code;
+    @NotEmpty(message = "{error.Empty}")
+    @Size(max = 2000, message = "{error.Max1000}")
     private String description;
+    @NotEmpty(message = "{error.Empty}")
     private String measure;
+    @NotNull(message = "{error.Empty}")
+    @Min(value = 0, message = "{error.Negative}")
     private Double price;
+    @NotNull(message = "{error.Empty}")
+    @Min(value = 0, message = "{error.Negative}")
     private Double ivaFee;
 
+    //-----------------------------------------------------------------------------------------
+    //Relaciones
+    //-----------------------------------------------------------------------------------------
     @ManyToOne
     @JoinColumn(name = "factura_code")
     @ToString.Exclude
