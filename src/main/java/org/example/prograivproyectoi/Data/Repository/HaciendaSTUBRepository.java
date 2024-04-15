9bc94966c83ac7221a194fd69cd652a1909187b5
JerrySC68 <jerrysc68@gmail.com>
2024-04-14 23:29:36 -0600

    Agrego clientes

--- src/main/java/org/example/prograivproyectoi/Data/Repository/HaciendaSTUBRepository.java
+++ src/main/java/org/example/prograivproyectoi/Data/Repository/HaciendaSTUBRepository.java
@@ -25,8 +25,10 @@ public class HaciendaSTUBRepository {
         Cliente pcliente;
 
         clientes.add(new Cliente("117010975", TipoCliente.FISICO, "Rodrigo Castro Rojas", "San Jose", "88888888", "rodrigo@gmail.com", null));
-        clientes.add(new Cliente("3102088049",TipoCliente.JURIDICO, "Suplidora Electromecanica","Heredia,Costa Ricas", "22601006","heredia@gdiez.com",null));
-
+        clientes.add(new Cliente("3102088049",TipoCliente.JURIDICO, "Suplidora Electromecanica","Heredia,Costa Rica", "22601006","heredia@gdiez.com",null));
+        clientes.add(new Cliente("901170162",TipoCliente.FISICO,"Jerry Solera Celestino","Heredia Costa Rica","84396547","jerrysc@gmail.com",null));
+        clientes.add(new Cliente("115960719",TipoCliente.FISICO, "Eddy Villarreal Muñoz","Alajuela, Costa Rica", "87532884","eddy.villarreal.munoz@est.una.ac.cr",null));
+        clientes.add(new Cliente("118510810",TipoCliente.JURIDICO, "Felipe Herrera Granados", "San Jose", "84243807", "felipe@gmail", null));
         System.out.println("Hacienda num elements: " + clientes.size());
     }
 
